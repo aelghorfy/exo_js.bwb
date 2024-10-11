950d581421670c2ca2164beb1c653d02f409898a
aelghorfy <kcmamin9@gmail.com>
2024-10-11 09:59:42 +0200

    constante&variable

---
+++ index.js
@@ -0,0 +1,11 @@
+// constante
+let monCanard ="Donald";
+const nombreDePate=2;
+const nombreDAiles=2;
+// variable
+function canard(nombreDePate,nombreDAiles){
+let maCanette="Daisy";
+let mesCanards=maCanette+=monCanard;
+return mesCanards;
+}
+console.log(canard());
