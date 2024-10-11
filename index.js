968ae84001fba8628b252d138fa3c398cc2063e2
aelghorfy <kcmamin9@gmail.com>
2024-10-11 15:53:20 +0200

    generateur plaque immatriculation

--- index.js
+++ index.js
@@ -10,11 +10,57 @@ return nombreDePattes+nombreDAiles;
 }
 console.log(canard(nombreDePattes,nombreDAiles)) */
 
-let x=1;
+/*let x=1;
 function f(){
 let x=2
 console.log(x);
 
 }
 console.log(x);
-f();
+f();*/
+
+const lettres="ABCDEFGHJKLMNPQRSTUVWXYZ";
+const nombres="0123456789";
+
+
+let nbplaque=prompt("jsp");
+let stockplaque=[];
+
+function generateLetter(){
+    let plaque="";
+    for ( i=0;i<2;i++){
+        plaque+=lettres.charAt(Math.floor(Math.random()*(lettres.length)));
+        
+    }
+    
+   return plaque;
+}
+
+function generateNumber(){
+    let plaque="-";
+    for ( i=0;i<3;i++){
+        plaque+=nombres.charAt(Math.floor(Math.random()*(nombres.length)));
+    }
+
+return plaque += "-";
+}
+
+function genererPlaques(){
+    let i=0;
+    while (i<50)
+    {
+        console.log(generateLetter()+generateNumber()+generateLetter());
+        i++;
+        console.log(i);
+    }
+
+}
+
+genererPlaques();
+
+/* let plaque = generateLetter() + generateNumber() + generateLetter ();
+
+for (let i=0;i<nbplaque;i++)
+    stockplaque.push(plaque);
+
+console.log(stockplaque) */
