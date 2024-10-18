a74acea656cddf379b97aa7b9ffda854d1982749
aelghorfy <kcmamin9@gmail.com>
2024-10-18 16:28:35 +0200

    Comparer Liste plus grande (+corrections)

--- index.js
+++ index.js
@@ -262,7 +262,7 @@ if (estPalindrome(motUtilisateur)) {
     
     //s, d, e = source, destination, extra
     
-    function tourDeHanoi(s, d, e, n) {
+   /*  function tourDeHanoi(s, d, e, n) {
 
         if (n <= 0) {
             return
@@ -273,11 +273,15 @@ if (estPalindrome(motUtilisateur)) {
 
     tourDeHanoi(e, d, s, n - 1);
 }
-tourDeHanoi('s', 'd', 'e', 3) 
+tourDeHanoi('s', 'd', 'e', 3)  */
+
+
+
+
 
      /* function genererMotif() {
        
-        const nombreLignes = parseInt(prompt("Entrez le nombre de lignes à générer : "));
+        const nombreLignes = parseInt(window.prompt("Entrez le nombre de lignes à générer : "));
         
        
 
@@ -308,49 +312,47 @@ console.log(indices);
  */
 
 
-/* function decaleMot (mot, n) {
+ /* function decaleMot (mot, n) {
 
     function decaleLettre(lettre, n) {
-    lettre = lettre.toLowerCase();
+        lettre = lettre.toLowerCase();
 
-    let code = lettre.charCodeAt(0);
+        let code = lettre.charCodeAt(0);
+        let a = 'a'.charCodeAt(0);
+        let z = 'z'.charCodeAt(0);
 
-    if (code>= 97 && code <= 122) {
-        code = ((code -97 + n) % 26) + 97;
-
+        if (code >= a && code <= z) {
+            code = ((code - a + n) % 26) + a;
+        }
+        return String.fromCharCode(code);
     }
-    return String.fromCharCode(code);
 
-}
-
-let resultat = '';
-for (let i = 0; i<mot.length; i++) {
-    resultat += decaleLettre(mot[i], n);
-}
-return resultat;
+    let resultat = '';
+    for (let i = 0; i < mot.length; i++) {
+        resultat += decaleLettre(mot[i], n);
+    }
+    return resultat;
 }
 
 let mot = window.prompt("Entrer un mot :");
+let n = parseInt(prompt("Entrer le nombre de décalage :"));
 
-let n = parseInt(prompt("Entrer le nombre de décalage :"), 10);
+console.log(decaleMot(mot, n)); */ 
 
-console.log("Le mot est :" + decaleMot(mot, n)); */
 
 /* function afficheEtoilesSansRepeat() {
    
     for (let i = 9; i >= 1; i--) {
       let ligne = ""; 
-      
-     
       for (let j = 0; j < i; j++) {
         ligne += "* "; 
       }
       
-      console.log(ligne.trim());
+      console.log(ligne);
     }
   }
   
-  afficheEtoilesSansRepeat(); */
+  afficheEtoilesSansRepeat();  */
 
 
  /*  function afficheEtoilesSansRepeat() {
@@ -363,7 +365,7 @@ console.log("Le mot est :" + decaleMot(mot, n)); */
         ligne += "* "; 
       }
       
-      console.log(ligne.trim());
+      console.log(ligne);
     }
   }
   
@@ -381,25 +383,21 @@ console.log("Le mot est :" + decaleMot(mot, n)); */
   
   genererRire(n) */
 
- /*  function calculerFrais() {
-    const tarifDeBase = 0.6;
-    const reduction = 0.5;
+    /* function calculerFrais() {
+    
 
     let km = parseFloat(window.prompt("Entrez le nombre de km :"));
 
     let nbPassagers = parseInt(window.prompt("Entrez le nombre de passagers :"));
 
-    if (nbPassagers <= 0) {
-        console.log("Qui va conduire même?!");
-        return;
-    }
-
-    let montantTotal = km * tarifDeBase - (reduction * nbPassagers);
-
-    let prixParPassager = montantTotal / nbPassagers
     
+
+    let montantTotal = km * 0.6 
+
+    let prixParPassager = montantTotal / nbPassagers - ( 0.5);
+
     console.log("Montant total des frais : " + montantTotal.toFixed(2) + " €");
-    console.log("Prix par passager : " + prixParPassager.toFixed(2) + " €");
+   
 
     for (let i = 1; i <= nbPassagers; i++) {
         console.log("Prix pour le passager " + i + " : " + prixParPassager.toFixed(2) + " €");
@@ -407,60 +405,11 @@ console.log("Le mot est :" + decaleMot(mot, n)); */
 
   } 
 
-  calculerFrais(); */
+  calculerFrais();  */
 
  
- 
- /*  function trieBulleinverse(etudiants) {
-    let n = etudiants.length;
-    let swapped;
-
-    do {
-        swapped = false;
-
-        for (let i = n - 1; i > 0; i--) {
-            if (etudiants[i].nom.toLowerCase() < etudiants[i - 1].nom.toLowerCase()) {
-                [etudiants[i], etudiants[i - 1]] = [etudiants[i - 1], etudiants[i]];
-                swapped = true;
-            }
-        }
-       
-    } while (swapped);
-
-    return etudiants;
-}
-
-function filtrerEtudiants(etudiants) {
-    let etudiantsFiltres = [];
-    let sommeNotes = 0;
-    let compteur = 0;
-
-   // note sup à 15 
-    for (let i = 0; i < etudiants.length; i++) {
-        if (etudiants[i].note > 15) {
-            etudiantsFiltres.push(etudiants[i]);
-            sommeNotes += etudiants[i].note;
-            compteur++;
-        }
-    }
-
-   // trie orde alpha 
-    etudiantsFiltres = trieBulleinverse(etudiantsFiltres);
-
-    // moyenne notes 
-    let moyenne = (compteur > 0) ? (sommeNotes / compteur) : 0;
-
-    // resultats 
-    console.log("Notes supérieure à 15 :");
-    for (let i = 0; i < etudiantsFiltres.length; i++) {
-        console.log(etudiantsFiltres[i].nom + " : " + etudiantsFiltres[i].note);
-    }
-
-    console.log("Moyenne des notes : " + moyenne.toFixed(2));
-}
-
-// liste 
-let etudiants = [
+  // liste 
+/* let etudiants = [
     { nom: 'Agathe', note: 14 },
     { nom: 'Wlad', note: 17 },
     { nom: 'Yohann', note: 16 },
@@ -477,9 +426,102 @@ let etudiants = [
     { nom: 'Amin', note: 16 }
 ];
 
+function comparerNomsAvecOrdreDefinit(nomA, nomB, ordre) {
+    let i = 0;
+    let length = Math.min(nomA.length, nomB.length);
+
+    while (i < length) {
+        let charA = nomA[i];
+        let charB = nomB[i];
+
+
+        let positionA = -1;
+        let positionB = -1;
+
+        for (let i = 0; i< ordre.length; i++) {
+            if (ordre[i] === charA) {
+                positionA = i;
+            }
+            if (ordre[i] === charB) {
+                positionB = i;
+            }
+
+            if (positionA !== -1 && positionB !== -1){
+                break;
+            }
+        }
+
+      
+        if (positionA !== positionB) {
+            return positionA - positionB;
+        }
+
+        i++;
+    }
+
+    return nomA.length - nomB.length;
+}
+
+function trieBulleinverse(etudiants) {
+    let n = etudiants.length;
+    let swapped;
+
+  
+    let ordre = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz';
+
+    do {
+        swapped = false;
+
+        for (let i = n - 1; i > 0; i--) {
+            if (comparerNomsAvecOrdreDefinit(etudiants[i].nom, etudiants[i - 1].nom, ordre) < 0) {
+                [etudiants[i], etudiants[i - 1]] = [etudiants[i - 1], etudiants[i]];
+                swapped = true;
+            }
+        }
+    } while (swapped);
+
+    return etudiants;
+}
+
+
+
+function filtrerEtudiants(etudiants) {
+    let etudiantsFiltres = [];
+    let sommeNotes = 0;
+    let compteur = 0;
+
+   // note sup à 15 
+    for (let i = 0; i < etudiants.length; i++) {
+        if (etudiants[i].note > 15) {
+
+            etudiantsFiltres.push(etudiants[i]);
+
+            sommeNotes += etudiants[i].note;
+
+            compteur++;
+        }
+    }
+
+   // trie ordre alpha 
+    etudiantsFiltres = trieBulleinverse(etudiantsFiltres);
+
+    // moyenne des notes 
+    let moyenne = (compteur > 0) ? (sommeNotes / compteur) : 0;
+
+    // resultats 
+    console.log("Notes supérieure à 15 :");
+    for (let i = 0; i < etudiantsFiltres.length; i++) {
+        console.log(etudiantsFiltres[i].nom + " : " + etudiantsFiltres[i].note);
+    }
+
+    console.log("Moyenne des notes : " + moyenne.toFixed(2));
+}
+
+
+
 
 filtrerEtudiants(etudiants);
- */
+  */ 
 
 
 
@@ -518,3 +560,164 @@ filtrerEtudiants(etudiants);
 }
 
 estImposable(); */  
+
+
+
+
+
+
+
+
+const tableau = [
+    { nom: "Alice", note: 85 },
+    { nom: "benjamin", note: 92 },
+    { nom: "Claire", note: 78 },
+    { nom: "david", note: 88 },
+    { nom: "Emma", note: 95 },
+    { nom: "fabien", note: 67 },
+    { nom: "Gabriel", note: 90 },
+    { nom: "helene", note: 73 },
+    { nom: "Isabelle", note: 82 },
+    { nom: "jacques", note: 76 },
+    { nom: "Kevin", note: 89 },
+    { nom: "laura", note: 91 },
+    { nom: "Marc", note: 84 },
+    { nom: "nathalie", note: 79 },
+    { nom: "Olivier", note: 87 },
+    { nom: "pierre", note: 93 },
+    { nom: "Quentin", note: 80 },
+    { nom: "roxane", note: 77 },
+    { nom: "Sophie", note: 94 },
+    { nom: "thomas", note: 86 },
+    { nom: "Ulysse", note: 81 },
+    { nom: "valerie", note: 75 },
+    { nom: "William", note: 83 },
+    { nom: "xavier", note: 70 },
+    { nom: "Yann", note: 88 },
+    { nom: "zoe", note: 92 },
+    { nom: "Adrien", note: 85 },
+    { nom: "brigitte", note: 78 },
+    { nom: "Camille", note: 89 },
+    { nom: "damien", note: 91 },
+    { nom: "Elise", note: 87 },
+    { nom: "françois", note: 76 },
+    { nom: "Gaëlle", note: 84 },
+    { nom: "hugo", note: 93 },
+    { nom: "Ingrid", note: 79 },
+    { nom: "julien", note: 88 },
+    { nom: "Karine", note: 82 },
+    { nom: "lucas", note: 90 },
+    { nom: "Mathilde", note: 95 },
+    { nom: "nicolas", note: 77 },
+    { nom: "Océane", note: 86 },
+    { nom: "paul", note: 80 },
+    { nom: "Quentin", note: 83 },
+    { nom: "rachel", note: 75 },
+    { nom: "Samuel", note: 92 },
+    { nom: "thibault", note: 89 },
+    { nom: "Ursula", note: 78 },
+    { nom: "vincent", note: 91 },
+    { nom: "Wendy", note: 87 },
+    { nom: "xena", note: 76 },
+    { nom: "Yasmine", note: 84 },
+    { nom: "zacharie", note: 93 },
+    { nom: "Amélie", note: 79 },
+    { nom: "bruno", note: 88 },
+    { nom: "Chloé", note: 82 },
+    { nom: "dylan", note: 90 },
+    { nom: "Estelle", note: 95 },
+    { nom: "fabrice", note: 77 },
+    { nom: "Géraldine", note: 86 },
+    { nom: "henri", note: 80 },
+    { nom: "Inès", note: 83 },
+    { nom: "jean", note: 75 },
+    { nom: "Kévin", note: 92 },
+    { nom: "léa", note: 89 },
+    { nom: "Mélanie", note: 78 },
+    { nom: "nathan", note: 91 },
+    { nom: "Ophélie", note: 87 },
+    { nom: "patrick", note: 76 },
+    { nom: "Quentin", note: 84 },
+    { nom: "rémi", note: 93 },
+    { nom: "Sarah", note: 79 },
+    { nom: "théo", note: 88 },
+    { nom: "Ugo", note: 82 },
+    { nom: "valentin", note: 90 },
+    { nom: "Wendy", note: 95 },
+    { nom: "xavier", note: 77 },
+    { nom: "Yohan", note: 86 },
+    { nom: "zoé", note: 80 },
+    { nom: "Antoine", note: 83 },
+    { nom: "bernadette", note: 75 },
+    { nom: "Cédric", note: 92 },
+    { nom: "diane", note: 89 },
+    { nom: "Élodie", note: 78 },
+    { nom: "franck", note: 91 },
+    { nom: "Guillaume", note: 87 },
+    { nom: "hélène", note: 76 },
+    { nom: "Isabelle", note: 84 },
+    { nom: "jules", note: 93 },
+    { nom: "Kevin", note: 79 },
+    { nom: "laetitia", note: 88 },
+    { nom: "Maxime", note: 82 },
+    { nom: "nadine", note: 90 },
+    { nom: "Olivier", note: 95 },
+    { nom: "pascal", note: 77 },
+    { nom: "Quentin", note: 86 },
+    { nom: "romain", note: 80 },
+    { nom: "Stéphanie", note: 83 },
+    { nom: "thomas", note: 75 },
+    { nom: "Ulysse", note: 92 },
+    { nom: "valérie", note: 89 }
+];
+
+function comparerNoms(nomA, nomB) {
+    let minLength = Math.min(nomA.length, nomB.length);
+    
+    for (let i = 0; i < minLength; i++) {
+        let charA = nomA[i];
+        let charB = nomB[i];
+
+        if (charA === 'é') charA = 'e';
+        if (charB === 'é') charB = 'e';
+        if (charA === 'É') charA = 'E';
+        if (charB === 'É') charB = 'E';
+
+
+        if (charA < charB) {
+            return -1;
+        }
+        if (charA > charB) {
+            return 1;
+        }
+    }
+
+    
+    return nomA.length - nomB.length;
+}
+
+
+function trieBulle(tableau) {
+    let n = tableau.length;
+    let swapped;
+
+    do {
+        swapped = false;
+        for (let i = 0; i < n - 1; i++) {
+            if (comparerNoms(tableau[i].nom, tableau[i + 1].nom) > 0) {
+                [tableau[i], tableau[i + 1]] = [tableau[i + 1], tableau[i]];
+                swapped = true;
+            }
+        }
+    } while (swapped);
+
+    return tableau;
+}
+
+
+let tableauTrie = trieBulle(tableau);
+
+
+for (let i = 0; i < tableauTrie.length; i++) {
+    console.log(tableauTrie[i].nom + " : " + tableauTrie[i].note);
+}
